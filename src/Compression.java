986c59ec72f288894206015db37a0554fa3e27b6
TheMissingText <dowlingscott016@gmail.com>
2023-01-10 14:40:15 +0000

    Check if a binary instance is within the current build

--- src/Compression.java
+++ src/Compression.java
@@ -20,16 +20,29 @@ public class Compression {
     public static ArrayList<String> convertBin(byte[] data){
         ArrayList<String> binaryArray = new ArrayList<>();
 
-        for(int item = 0; item<data.length; item++){
+        for (int item : data) {
             String binary = "";
-            int number = data[item];
-            while (number>0){
-                int remainder = number%2;
-                binary = remainder+binary;
-                number/=2;
+            int number = item;
+            while (number > 0) {
+                int remainder = number % 2;
+                binary = remainder + binary;
+                number /= 2;
             }
             binaryArray.add(binary);
         }
         return binaryArray;
+    }
+    public static void compress(ArrayList<String> binaryArray){
+        ArrayList<String> finalCompression = new ArrayList<>();
+
+        }
+    }
+    public static boolean checkUsed(ArrayList<String> current, String checkFor){
+        for (String item : current){
+            if (item == checkFor){
+                return true;
+            }
+        }
+        return false;
     }
 }
